8afef1f4989cd57600b3defd1099a597353b1465
Ishu2801 <72581419+Ishu2801@users.noreply.github.com>
2021-09-29 21:21:59 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Slideshow from './component';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="slideshow-container">
+        <Slideshow />
+        </div>
+    </div>
+  );
+}
+
+export default App;
